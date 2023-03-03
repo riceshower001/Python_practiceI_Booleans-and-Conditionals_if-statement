3abc13df37558cd609dfe836525d205cfd4ed9a7
riceshower001 <125946802+riceshower001@users.noreply.github.com>
2023-03-03 19:57:04 +0900

    Add files via upload

---
+++ Python_practiceI_Booleans and Conditionals_if statement.py
@@ -0,0 +1,10 @@
+x = 7 * 10
+y = 5 * 6
+
+# Please print "x is 70" if "x" equal to 70.
+if x == 70 :
+  print("x is 70")
+
+# Please print "y is not 40" if "y" is not equal to 40.
+if y != 40 :
+  print("y is not 40")
